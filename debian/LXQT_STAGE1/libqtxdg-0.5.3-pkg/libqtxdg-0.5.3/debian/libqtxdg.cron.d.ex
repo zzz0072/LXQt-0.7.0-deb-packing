c225e55106b7621fe2a2b2cf61858bfa0230c3c4
Wen.Liao <zzz00072@gmail.com>
2014-08-06 01:32:18 +0800

    libqtxdg-0.5.3/debian: First version to get debuild -us -uc checking has no error

--- debian/LXQT_STAGE1/libqtxdg-0.5.3-pkg/libqtxdg-0.5.3/debian/libqtxdg.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libqtxdg package
-#
-0 4	* * *	root	[ -x /usr/bin/libqtxdg_maintenance ] && /usr/bin/libqtxdg_maintenance
--- debian/LXQT_STAGE1/libqtxdg-0.5.3-pkg/libqtxdg-0.5.3/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libqtxdg):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libqtxdg" command="/usr/bin/libqtxdg"
