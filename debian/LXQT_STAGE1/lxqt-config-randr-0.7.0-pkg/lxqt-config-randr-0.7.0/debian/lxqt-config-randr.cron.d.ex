5fc33eb82c7b73fdc517bf14f7a269120b49f505
Wen.Liao <zzz00072@gmail.com>
2014-08-06 15:07:02 +0800

    lxqt-config-randr-0.7.0: Modify debian/ to get pbuilder test pass

--- debian/LXQT_STAGE1/lxqt-config-randr-0.7.0-pkg/lxqt-config-randr-0.7.0/debian/lxqt-config-randr.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the lxqt-config-randr package
-#
-0 4	* * *	root	[ -x /usr/bin/lxqt-config-randr_maintenance ] && /usr/bin/lxqt-config-randr_maintenance
--- debian/LXQT_STAGE1/lxqt-config-randr-0.7.0-pkg/lxqt-config-randr-0.7.0/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(lxqt-config-randr):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="lxqt-config-randr" command="/usr/bin/lxqt-config-randr"
